4bc6c03560ac00b2ed7b762b7404de9c5b744002
Dylan Domingo <113019074+dylandomingo57@users.noreply.github.com>
2023-12-19 12:02:49 -1000

    Create init.CameraShake.lua

---
+++ src/client/init.CameraShake.lua
@@ -0,0 +1,43 @@
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local remoteEvents = ReplicatedStorage.RemoteEvents
+local camShakeEvent = remoteEvents["ShakeCam Event"]
+local camShakeEventDistance = remoteEvents["ShakeCamDistance Event"]
+
+plr = game.Players.LocalPlayer
+
+camShakeEvent.OnClientEvent:Connect(function(shakes)
+	local char = plr.Character or plr.CharacterAdded:Wait()
+	local hum = char:FindFirstChild("Humanoid") 
+	for i = 1, shakes do
+		--print("offsetting cam")
+		hum.CameraOffset = Vector3.new(math.random(-120,120)/120, math.random(-120,120)/120, math.random(-120,120)/120)
+		wait(.05)
+	end
+	hum.CameraOffset = Vector3.new(0,0,0)
+end)
+
+camShakeEventDistance.OnClientEvent:Connect(function(shakes, obj)
+	--print(shakes.." "..tostring(obj))
+	local char = plr.Character or plr.CharacterAdded:Wait()
+	local hum = char:FindFirstChild("Humanoid")
+	local root = char:FindFirstChild("HumanoidRootPart")
+	
+	local rootPos = root.Position
+	local objPos = obj.Position
+	
+	local distance = (rootPos - objPos).Magnitude / 100
+	if distance < 0.1 then
+		distance = 1
+	elseif distance > 200 then
+		return
+	end
+	print(distance)
+	
+	for i = 1, shakes do
+		--print("offsetting cam")
+		hum.CameraOffset = Vector3.new(math.random(-120,120)/(120 * distance), math.random(-120,120)/(120 * distance), math.random(-120,120)/(120 * distance))
+		--print(hum.CameraOffset)
+		wait(.05)
+	end
+	hum.CameraOffset = Vector3.new(0,0,0)
+end)
