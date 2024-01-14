3eccf84961d3b899493060a485418c8d75b9ab53
Dylan Domingo <113019074+dylandomingo57@users.noreply.github.com>
2024-01-14 12:22:13 -1000

    altitude tracker should be replicated across server now

--- src/client/init.AltitudeTracker.lua
+++ src/client/init.AltitudeTracker.lua
@@ -1,7 +1,5 @@
 local RunService = game:GetService("RunService")
---local ReplicatedStorage = game:GetService("ReplicatedStorage")
---local UpdateAltitudeEvent = ReplicatedStorage.RemoteEvents.PlayerEvents.UpdateAltitude
-local PlayerStatUpdater = require(game:GetService("ReplicatedStorage").Shared.PlayerStatUpdater)
+local StatChangeEvent = game:GetService("ReplicatedStorage").RemoteEvents["StatChange Event"]
 local Players = game:GetService("Players")
 
 local yOffSet = 52
@@ -9,16 +7,14 @@ local altitude
 
 local player = game.Players.LocalPlayer
 local char = player.Character or player.CharacterAdded:Wait()
---local hum = char.Humanoid or char:WaitForChild("Humanoid")
 
 RunService.Stepped:Connect(function()
-	--print(math.floor(char.HumanoidRootPart.Position.Y - yOffSet))
 	local root
 	pcall(function()
 		root = char.HumanoidRootPart
 	end)
 	if root ~= nil then
 		altitude = root.Position.Y - yOffSet
-		PlayerStatUpdater.updateAltitude(altitude, player)
+		StatChangeEvent:FireServer(altitude)
 	end
 end)
